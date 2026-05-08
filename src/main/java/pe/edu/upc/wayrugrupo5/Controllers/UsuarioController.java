463458966eb2fb9daf92d05ed3ef88040ef56d8c
leonel <lj6730677@gmail.com>
2026-05-08 00:57:11 -0500

    cmabios

--- src/main/java/pe/edu/upc/wayrugrupo5/Controllers/UsuarioController.java
+++ src/main/java/pe/edu/upc/wayrugrupo5/Controllers/UsuarioController.java
@@ -4,6 +4,7 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import pe.edu.upc.wayrugrupo5.DTOS.UsuarioDTO;
 import pe.edu.upc.wayrugrupo5.Entities.Usuario;
@@ -70,6 +71,7 @@ public class UsuarioController {
     }
 
     @GetMapping("/listar")
+    @PreAuthorize("hasAuthority('cliente')")
     public List<UsuarioDTO> listar() {
         ModelMapper m = new ModelMapper();
         return uS.listarTodo().stream()
