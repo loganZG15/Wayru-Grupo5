47791efcf4cf0823e164a18376e95414652ccfe1
leonel <lj6730677@gmail.com>
2026-05-07 20:28:33 -0500

    actualizacion

--- src/main/java/pe/edu/upc/wayrugrupo5/Controllers/ComentarioController.java
+++ src/main/java/pe/edu/upc/wayrugrupo5/Controllers/ComentarioController.java
@@ -1,11 +1,16 @@
 package pe.edu.upc.wayrugrupo5.Controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import pe.edu.upc.wayrugrupo5.ServicesInterfaces.IComentarioService;
 
 @RestController
 @RequestMapping("/api/comentario")
 public class ComentarioController {
+
+        @Autowired
+        private IComentarioService cS;
 
 
 }
