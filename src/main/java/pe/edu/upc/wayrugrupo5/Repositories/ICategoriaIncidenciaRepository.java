2710fcb381d6bc53850c24caf85ef2eca84f317e
loganZG15 <147010683+loganZG15@users.noreply.github.com>
2026-06-30 18:56:04 -0500

    Add method to find CategoriaIncidencia by name
    Added a method to find CategoriaIncidencia by name ignoring case.

--- src/main/java/pe/edu/upc/wayrugrupo5/Repositories/ICategoriaIncidenciaRepository.java
+++ src/main/java/pe/edu/upc/wayrugrupo5/Repositories/ICategoriaIncidenciaRepository.java
@@ -8,11 +8,14 @@ import org.springframework.stereotype.Repository;
 import pe.edu.upc.wayrugrupo5.Entities.CategoriaIncidencia;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface ICategoriaIncidenciaRepository extends JpaRepository<CategoriaIncidencia, Integer>
 {
     List<CategoriaIncidencia> findByNombreCategoriaContainingIgnoreCase(String nombreCategoria);
+
+    Optional<CategoriaIncidencia> findByNombreCategoriaIgnoreCase(String nombreCategoria);
 
     @Query("SELECT c FROM CategoriaIncidencia c WHERE c.subCategoria = :subCategoria")
     List<CategoriaIncidencia> findBySubCategoria(@Param("subCategoria") String subCategoria);
